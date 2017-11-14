3e393956540b20bcb7b6e5de4a451d0678ee89f4
licurini <licurini@gmail.com>
2017-11-14 16:33:40 +0100

    Created Profiles component

---
+++ src/Profiles.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Profiles.css';
+
+class Profiles extends Component {
+    constructor (props) {
+		super();
+		
+		this.state = {
+			
+		}
+	}
+  
+  
+    render() {
+        return (
+            <div className="Profiles">
+              
+            </div>
+        );
+    }
+}
+
+export default Profiles;
