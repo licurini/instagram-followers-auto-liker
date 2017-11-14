9325efd446359343f168a8045e11436aa183c87a
licurini <licurini@gmail.com>
2017-11-14 17:09:20 +0100

    Added users array to Profiles.js state

--- src/Profiles.js
+++ src/Profiles.js
@@ -6,6 +6,49 @@ class Profiles extends Component {
 		super();
 		
 		this.state = {
+			users: [
+			    {
+				    id: "1",
+					name: "Tomek",
+					email: "abc@abc.pl"
+			    },
+				{
+					id: "2",
+					name: "Dawid",
+					email: "aqw@abc.pl"
+				},
+				{
+					id: "3",
+					name: "Marcin",
+					email: "qwe@qwe.pl"
+				},
+				{
+					id: "4",
+					name: "Jakub",
+					email: "asfasf@asda.pl"
+				},
+				{
+					id: "5",
+					name: "Mateusz",
+					email: "adads@adad.pl"
+				},
+				{
+					id: "6",
+					name: "Wojciech",
+					email: "adsf@fvds.pl"
+				},
+				{
+					id: "7",
+					name: "Adrian",
+					email: "sdfasg@ghd.pl"
+				},
+				{
+					id: "8",
+					name: "Arkadiusz",
+					email: "asa@fsfs.pl"
+				},
+			]
+				
 			
 		}
 	}
