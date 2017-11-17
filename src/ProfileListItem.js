13bd79d3d3f572685b2b4a7ae7f1bd01a38e8801
licurini <licurini@gmail.com>
2017-11-17 16:55:14 +0100

    updated ProfileListItem

--- src/ProfileListItem.js
+++ src/ProfileListItem.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import './ProfileListItem.css';
 
 class ProfileListItem extends Component {
+	constructor (props) {
+		super ();
+		
+		this.state = {
+			id: "",
+			name: "",
+			email: ""
+		}
+	    
+		console.log(this.state)	
+	}
+
+	/* Zapisuje do stanu komponentu propsy przekazane przez ProfileList */
+	componentWillMount () {
+		this.setState({
+			id: this.props.id,
+			name: this.props.name,
+			email: this.props.email
+		})
+		
+	}
 	
-	
+    componentDidMount () {
+		console.log(this.state)
+	}	
 	render () {
 		return (
 		    <div>
